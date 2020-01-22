9abec32637c367572789b94056841b67744bfb92
jmpiontek <jeremypiontek1@gmail.com>
2020-01-22 17:16:31 -0600

    Updates

---
+++ precache-manifest.2ea8b11de82ffec537e82dae50e17722.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "97fd0aafbee3d1e61d45a9732bf6b608",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "ccc514772158dc17284a",
+    "url": "/monsters-rolodex/static/css/main.e7820008.chunk.css"
+  },
+  {
+    "revision": "c68e14fdc7e1ba4a5621",
+    "url": "/monsters-rolodex/static/js/2.9e7d0785.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/monsters-rolodex/static/js/2.9e7d0785.chunk.js.LICENSE"
+  },
+  {
+    "revision": "ccc514772158dc17284a",
+    "url": "/monsters-rolodex/static/js/main.872f09b4.chunk.js"
+  },
+  {
+    "revision": "9e014af7b12dc10ef867",
+    "url": "/monsters-rolodex/static/js/runtime-main.ff038b7c.js"
+  }
+]);
